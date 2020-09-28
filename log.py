9b034440a3811ea315d8dd81f66e1a4a9f7b1d51
zinuok <zinuok@kaist.ac.kr>
2020-09-28 15:59:29 +0900

    Update log.py

--- log.py
+++ log.py
@@ -3,26 +3,51 @@
 
 # ref: https://thispointer.com/python-get-list-of-all-running-processes-and-sort-by-highest-memory-usage/
 
-from jtop import jtop, JtopException
-import csv
-import argparse
 import psutil
+import csv, argparse
 from time import sleep
+
+# variables
+proc_list = ['rqt_image_view']   # process name list
+interval = 0.1           # time interval for each logging
+
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save', action="store", dest="file", default="log.csv")
+    args = parser.parse_args()
+    
+    f = open(args.file, 'w')
+    wr = csv.writer(f)
+    wr.writerow(['CPU %', 'Mem %'])
     while True:
+        total_cpu = 0
+        total_mem = 0
         # Iterate over all running process
         for proc in psutil.process_iter():
             try:
                 # Get process info.
-                stats = jetson.stats
                 info = proc.as_dict(attrs=['name', 'cpu_percent', 'memory_percent'])
-                if info['name'] == "rqt_image_view":
-                    print(info)
+                if info['name'] in proc_list:
+                    # calculate total CPU and Memory usage
+                    total_cpu += info['cpu_percent']
+                    total_mem += info['memory_percent']
+                    
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
-        sleep(0.1)
+            except KeyboardInterrupt:
+                print("Crtl+'C' interrupt")
+                close(f)
+        
+        # log
+        #print("cpu: ", total_cpu, "mem: ", total_mem)
+        wr.writerow([total_cpu, total_mem])
+        sleep(interval)     
+    
+    close(f)
+
+
 
 
 if __name__ == '__main__':
